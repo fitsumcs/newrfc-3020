38a1aa3d111499ab872d52590f72dadd019d79c9
Fitsum A <fitsumcs@gmail.com>
2021-07-07 14:02:03 +0300

    check for loaing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
+import { useState} from 'react';
 import './App.css';
 import CoverImageView from './CoverImageView'
 
-const error = true
+
 
 function App() {
+
+  const [error , setError] =  useState(true) 
+  const [loading , setLoading] =  useState(false) 
+
+  const changeError=()=>{
+    setLoading(true)
+    console.log(loading);
+    
+    setTimeout(()=>{
+      setLoading(false)
+      setError(false)
+    },3000)
+
+  }
+
   return (
     <div style={{display: 'flex', flexDirection: 'column', backgroundColor: '#fafafa'}}>
     <CoverImageView imgSrc={'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg'} />
-    {error ? 
-      <p style={{ paddingLeft: 16, color: '#d8000c' }}>
+    
+    {error ?
+     <div style={{
+       display: 'flex',   
+        marginLeft: 32, 
+        marginRight: 32, 
+        marginTop:100,
+        padding: 16
+        
+        }}> 
+      <p style={{ paddingLeft: 16,  marginLeft: 32, color: '#d8000c' }}>
         {'Unexpected error'}
-      </p> : 
+      </p>
+      <button onClick={changeError}>refresh</button>
+
+     </div>
+    :  (loading ?  <p>Loading</p>:
       <div style={{
         marginLeft: 32, 
         marginRight: 32, 
@@ -25,6 +54,7 @@ function App() {
         <h2>{'Card Title'}</h2>
         <p>{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`}</p>
       </div>
+    )
       }
   </div>
   );
